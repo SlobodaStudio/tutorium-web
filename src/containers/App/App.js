d2382140d2d0807142b2ef943d3e1e5c6abc8ad5
Victor Zagorodny <post.vittorius@gmail.com>
2017-01-12 13:42:48 +0200

    Add Sloboda logo

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
-import './App.css'
+import styles from './App.scss'
 
 import AppBar from 'react-toolbox/lib/app_bar'
 import Navigation from 'react-toolbox/lib/navigation'
 import Link from 'react-toolbox/lib/link'
 import logoImg from './sloboda-logo.png'
 
+
 class App extends Component {
   render() {
-    const SlobodaLogo = () => <img src={logoImg} alt='logo' />
+    const SlobodaLogo = () => <img src={logoImg} className={styles.SlobodaLogo} alt='logo' />
 
     return (
       <AppBar title='Tutorium' leftIcon={<SlobodaLogo />}>
