3e316df9ca9cda9a1247b968603e4725bf594ff6
limoozyn <limoozyn@gmail.com>
2017-05-04 19:36:04 +0300

    Add fake hash of dates with events

--- src/containers/main_area/MainArea.js
+++ src/containers/main_area/MainArea.js
@@ -1,8 +1,36 @@
 import React from 'react'
 
 class MainArea extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dates: {
+        1: {
+          date: "March, 10",
+          events: {
+            1: "Event 1",
+          }
+        },
+        2: {
+          date: "April, 5",
+          events: {
+            1: "Event 1",
+            2: "Event 2"
+          }
+        }
+      }
+    };
+  }
   render() {
-    return null
+    return (
+      <div className="event-list">
+        <h1>Upcoming Events</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
   }
 }
 
