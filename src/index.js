f20ae7cee6088d4db6767bfb6a26e76a63d91587
limoozyn <limoozyn@gmail.com>
2017-05-04 19:36:04 +0300

    Add fake hash of dates with events

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/app/App';
+import App from './containers/App/App';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducer from 'redux/modules/root_reducer'
