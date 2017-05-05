63f1026f3764d12bcdd4e77f174d9167dbe3f89b
limoozyn <limoozyn@gmail.com>
2017-05-05 19:33:23 +0300

    Add fake events list to the main area

--- src/containers/main_area/MainArea.js
+++ src/containers/main_area/MainArea.js
@@ -1,36 +1,53 @@
 import React from 'react'
 
+function Day(props) {
+  const listEvents = props.dateAndEvents.events.map((event, index) =>
+    <li key={index}>{event}</li>
+  );
+  return (
+    <li key={props.dateAndEvents.date}>
+      <h2>{props.dateAndEvents.date}</h2>
+      <ul>{listEvents}</ul>
+    </li>
+  );
+};
+
+function DaysList(props) {
+  const listDays = props.dates.map((aDate) =>
+    <Day dateAndEvents={aDate} />
+  );
+  return (
+    <ul className="event-list">
+      {listDays}
+    </ul>
+  );
+};
+
 class MainArea extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      dates: {
-        1: {
+    this.state = {dates: [
+        {
           date: "March, 10",
-          events: {
-            1: "Event 1",
-          }
+          events: [
+            "Machine learning practice"
+          ]
         },
-        2: {
+        {
           date: "April, 5",
-          events: {
-            1: "Event 1",
-            2: "Event 2"
-          }
+          events: [
+            "Ruby vs JavaScript: be quick or be dead",
+            "Super Duper JavaScript"
+          ]
         }
-      }
+      ]
     };
   }
   render() {
     return (
-      <div className="event-list">
-        <h1>Upcoming Events</h1>
-        <ul>
-          <li>Instagram</li>
-          <li>WhatsApp</li>
-          <li>Oculus</li>
-        </ul>
-      </div>
+      // <h1>Upcoming Events</h1>
+      <DaysList dates={this.state.dates} />
+    );
   }
 }
 
