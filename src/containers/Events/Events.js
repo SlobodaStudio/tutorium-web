8950f70a82c894c015fba7b5e871b67c56413d31
limoozyn <limoozyn@users.noreply.github.com>
2017-05-10 19:45:57 +0300

    Vm edu 7 event feed component (#4)
    * Add fake hash of dates with events
    
    * Add fake events list to the main area
    
    * Use React Toolbox to show list
    
    * Update as per change requests from Roman
    
    * Update as per Roman's comments

---
+++ src/containers/Events/Events.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { List} from 'react-toolbox/lib/list'
+import Day from '../../components/Day/Day'
+import styles from './Events.scss'
+
+class Events extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {dates: [
+        {
+          date: "March, 10",
+          events: [
+            "Machine learning practice"
+          ]
+        },
+        {
+          date: "April, 5",
+          events: [
+            "Ruby vs JavaScript: be quick or be dead",
+            "Super Duper JavaScript"
+          ]
+        }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div className={styles.container__list}>
+        <h3>Upcoming Events</h3>
+        <List>
+          {this.state.dates.map(date =>
+            <Day dateAndEvents={date} />
+          )}
+        </List>
+      </div>
+    );
+  }
+}
+
+export default Events
--- src/containers/main_area/MainArea.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-class MainArea extends React.Component {
-  render() {
-    return null
-  }
-}
-
-export default MainArea
