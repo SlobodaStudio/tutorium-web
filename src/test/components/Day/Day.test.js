5a1ec9c8e391e30bf511b9d6629cee886c4031fd
limoozyn <limoozyn@gmail.com>
2017-05-12 16:10:22 +0300

    test: Add snapshot tests for Events and Day component

--- src/test/components/Day/Day.test.js
+++ src/test/components/Day/Day.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { List, ListItem, ListSubHeader} from 'react-toolbox/lib/list'
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import Day from '../../../components/Day/Day'
 
@@ -27,4 +28,11 @@ test('can be rendered', () => {
 test('renders `ListItem`s', () => {
   const wrapper = shallow(<Day key={date.date} dateAndEvents={date} />);
   expect(wrapper.find('.day-сontent').length).toBe(1);
+});
+
+test('Day renders correctly', () => {
+  const tree = renderer.create(
+    <Day key={date.date} dateAndEvents={date}/>
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
--- src/test/containers/Events/Events.test.js
+++ src/test/containers/Events/Events.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { List, ListItem, ListSubHeader} from 'react-toolbox/lib/list'
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import Events from '../../../containers/Events/Events'
 import Day from '../../../components/Day/Day'
@@ -22,4 +23,11 @@ test('can be rendered', () => {
 test('renders two <Day /> components', () => {
   const wrapper = shallow(<Events />);
   expect(wrapper.find(Day).length).toBe(2);
+});
+
+test('Events renders correctly', () => {
+  const tree = renderer.create(
+    <Events />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
